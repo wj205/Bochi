761cf9fc7e774db2f947520b2a3cedf25e023598
Sebastian Herscher <herscher.sebastian@gmail.com>
2015-07-26 13:52:35 -0700

    LevelController, Fader, and start of Target

---
+++ Assets/Scripts/Scripts - Sebastian/Target.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Target : MonoBehaviour {
+
+    private LevelController _levelController;
+    private Fader _fader;
+    private Collider2D _collider;
+
+    private TargetState state = TargetState.UNHIT;
+
+	// Use this for initialization
+	void Start () {
+        this._levelController = GameObject.FindObjectOfType<LevelController>();
+        this._fader = this.GetComponent<Fader>();
+        this._collider = this.GetComponent<Collider2D>();
+        this.state = TargetState.UNHIT;
+	}
+
+
+    void SwitchToState(TargetState s)
+    {
+        this.state = s;
+        switch (this.state)
+        {
+            case TargetState.UNHIT:
+                this.SwitchToUnhit();
+                break;
+            case TargetState.HIT:
+                this.SwitchToHit();
+                break;
+        }
+    }
+
+    protected virtual void SwitchToUnhit()
+    {
+        this._fader.SwitchToState(FadeState.IN);
+        this._collider.enabled = true;
+    }
+
+    protected virtual void SwitchToHit()
+    {
+        this._fader.SwitchToState(FadeState.OUT);
+        this._collider.enabled = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            this.SwitchToHit();
+            _levelController.CheckForWinCondition();
+        }
+    }
+
+    private enum TargetState
+    {
+        UNHIT, HIT
+    }
+}
