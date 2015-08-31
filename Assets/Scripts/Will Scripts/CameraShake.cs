a995abf57f8d8a6e7c00b650dca4a69c08ef728b
Will Jeffers <wfj205@gmail.com>
2015-08-31 16:34:54 -0400

    added a shake strength to the camera shake, to change the intensity of the screen shake for different objects

--- Assets/Scripts/Will Scripts/CameraShake.cs
+++ Assets/Scripts/Will Scripts/CameraShake.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class CameraShake : MonoBehaviour {
 
 	public float shakeLength = 0.3f;
+	public float shakeStrength = 1f;
 	Vector3 baseCameraPosition;
 
 	void Start()
@@ -33,8 +34,8 @@ public class CameraShake : MonoBehaviour {
 		while(t > 0f)
 		{
 			Camera.main.transform.position = baseCameraPosition + t * new Vector3(
-				Mathf.Sin (Time.time * Random.Range (2.5f, 5f)),
-				Mathf.Sin (Time.time * Random.Range (2.5f, 5f)),
+				Mathf.Sin (Time.time * Random.Range (2.5f, 5f)) * shakeStrength,
+				Mathf.Sin (Time.time * Random.Range (2.5f, 5f)) * shakeStrength,
 				0f);
 			t -= Time.deltaTime;
 			yield return 0;
