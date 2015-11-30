671030a12fb038aa88994660f2d1b11f6f47ff2d
Will Jeffers <wfj205@gmail.com>
2015-11-30 17:38:48 -0600

    Added background art (space pics and asteroids)

---
+++ Assets/Scripts/Will Scripts/Asteroids.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Asteroids : MonoBehaviour {
+
+	bool movingRight;
+	float targetY;
+
+	float rotSpeedX;
+	float rotSpeedY;
+	float rotSpeedZ;
+	Vector3 rotateSpeed;
+
+	void Start()
+	{
+		if(this.transform.position.x < 0f)
+		{
+			//start out moving right
+			movingRight = true;
+		}else
+		{
+			//start out moving left
+			movingRight = false;
+		}
+		rotSpeedX = Random.Range (0f, 2f);
+		rotSpeedY = Random.Range (0f, 2f);
+		rotSpeedZ = Random.Range (0f, 2f);
+		rotateSpeed = new Vector3(rotSpeedX, rotSpeedY, rotSpeedZ);
+		targetY = Random.Range (-5f, 5f);
+	}
+
+	void Update()
+	{
+		if(movingRight)
+		{
+			Vector3 targetPos = new Vector3(15f, targetY, 5f);
+			this.transform.position = Vector3.MoveTowards (this.transform.position, targetPos, Time.deltaTime * Random.Range (2f, 5f));
+		}else
+		{
+			Vector3 targetPos = new Vector3(-15f, targetY, 5f);
+			this.transform.position = Vector3.MoveTowards (this.transform.position, targetPos, Time.deltaTime * Random.Range (2f, 5f));
+		}
+
+		this.transform.Rotate (rotateSpeed);
+
+		if(this.transform.position.x > 16 || this.transform.position.x < -16)
+		{
+			Destroy (this.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Will Scripts/BackgroundController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundController : MonoBehaviour {
+
+	public GameObject[] asteroids;
+
+	void Update()
+	{
+		SpawnAsteroids();
+	}
+
+
+	float spawnTimer = 0f;
+	float spawnMax = 3f;
+	void SpawnAsteroids()
+	{
+		if(spawnTimer < spawnMax)
+		{
+			spawnTimer += Time.deltaTime;
+		}else
+		{
+			SpawnAsteroid();
+			GetSpawnMax();
+			spawnTimer = 0f;
+		}
+	}
+
+	void GetSpawnMax()
+	{
+		spawnMax = Random.Range (3f, 8f);
+	}
+
+	void SpawnAsteroid()
+	{
+		float rand = Random.Range (0f, 1f);
+		float posY = Random.Range (-10f, 10f);
+		float posX = 0f;
+		if(rand > 0.5f)
+		{
+			posX = -15f;
+		}else
+		{
+			posX = 15f;
+		}
+		Vector3 asteroidPos = new Vector3(posX, posY, 5f);
+		GameObject newAsteroid = Instantiate (GetRandomAsteroid(), asteroidPos, Quaternion.identity) as GameObject;
+	}
+
+	GameObject GetRandomAsteroid()
+	{
+		int asteroidNum = asteroids.Length;
+		int randAsteroid = Random.Range (0, asteroidNum);
+		return asteroids[randAsteroid];
+	}
+}
